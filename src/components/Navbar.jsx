f80d810dd1e33cddde8cf348151145106034d464
gvstaov <gustavoavlis@icloud.com>
2024-10-31 17:44:16 -0300

    Save!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,46 @@
-import logo from "../assets/raviKumarLogo.webp"
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import logo from "../assets/raviKumarLogo.webp";
+import { FaSquareXTwitter } from "react-icons/fa6";
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between py-60">
+    <nav className="flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <a href="/" aria-label="Home">
           <img src={logo} className="mx-2" width={50} height={33} alt="Logo" />
         </a>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a href="https://www.linkedin.com/in/gustavoavlis"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.github.com/gvstaov"
-            target="_blank"
+        <a
+          href="https://www.linkedin.com/in/gustavoavlis"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/gvstaov"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Github"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/gvstaov"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://www.twitter.com/gvstaov"
+          rel="noopener noreferrer"
+          aria-label="Twitter"
+        >
+          <FaSquareXTwitter />
+        </a>
       </div>
     </nav>
   );
