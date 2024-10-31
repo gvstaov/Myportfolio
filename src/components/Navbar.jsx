64b95ec4ff9d4cbf20c97e3769cc3197c9a7de26
gvstaov <gustavoavlis@icloud.com>
2024-10-31 16:03:13 -0300

    Add navbar!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
+import logo from "../assets/raviKumarLogo.webp"
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between py-60">
       <div className="flex flex-shrink-0 items-center">
         <a href="/" aria-label="Home">
-          <img src={logo} />
+          <img src={logo} className="mx-2" width={50} height={33} alt="Logo" />
         </a>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/gustavoavlis"
+            target="_blank"
       </div>
     </nav>
   );
