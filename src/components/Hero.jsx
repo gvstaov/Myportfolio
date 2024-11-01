9fee84f7da261def8789f906b539b76e358c86fc
gvstaov <gustavoavlis@icloud.com>
2024-11-01 14:27:05 -0300

    Hero section updates!

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,15 @@ const Hero = () => {
       <div className="flex flex-wrap lg:flex-row-reverse">
         <div className="w-full lg:w-1/2">
           <div className="flex justify-center lg:p-8">
-            <img src={profilePic} alt="Gustavo Profile Picture" />
+            <img
+              src={profilePic}
+              alt="Gustavo Profile"
+              className="border border-stone-900 rounded-3x1"
+            />
           </div>
+        </div>
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start mt-10"></div>
         </div>
       </div>
     </div>
