5b09b31ffda42c268238a1e0b99b29013304fd9c
gvstaov <gustavoavlis@icloud.com>
2024-11-05 17:00:53 -0300

    Visual bug fixes!

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,7 +16,18 @@ const Projects = () => {
                 className="mb-6 rounded"
               />
             </div>
-            <div className="w-full max-w-xl lg:w-3/4"></div>
+            <div className="w-full max-w-xl lg:w-3/4">
+              <h3 className="mb-2 font-semibold text-2xl">{project.title}</h3>
+              <p className="mb-4 text-stone-400">{project.description}</p>
+              {project.technologies.map((tech, index) => (
+                <span
+                  className="mr-2 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300"
+                  key={index}
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
           </div>
         ))}
       </div>
