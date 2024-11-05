4305db38b30f9bdc9c0666f3726db9e91b7e5267
gvstaov <gustavoavlis@icloud.com>
2024-11-05 17:58:18 -0300

    New sections!

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -18,6 +20,8 @@ const App = () => {
         <Hero />
         <Technologies />
         <Projects />
+        <Experience />
+        <Contact />
       </div>
     </div>
   );
